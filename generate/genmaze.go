5b3df7656d666e751c642ad30f66921f05e4f7a1
Daniel Tulovsky <dant@imacz.lan>
2017-01-25 23:26:56 -0500

    Initial implementation of cells and grid.

--- generate/genmaze.go
+++ generate/genmaze.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"mazes/grid"
+)
+
 func main() {
-  
+
+	g := grid.NewGrid(10, 10)
+	fmt.Printf("%v\n", g)
+
+	for _, cell := range g.Cells() {
+		fmt.Printf("%v\n", cell)
+	}
+
 }
---
+++ grid/grid.go
@@ -0,0 +1,172 @@
+package grid
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano()) // need to initialize the seed
+}
+
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+type Grid struct {
+	rows    int
+	columns int
+	cells   [][]*Cell
+}
+
+// NewGrid returns a new grid.
+func NewGrid(r, c int) *Grid {
+	g := &Grid{
+		rows:    r,
+		columns: c,
+		cells:   [][]*Cell{},
+	}
+
+	g.prepareGrid()
+	g.configureCells()
+	return g
+}
+
+func (g *Grid) String() string {
+	return fmt.Sprintf("Grid size %v\n", g.Size())
+}
+
+func (g *Grid) prepareGrid() {
+	g.cells = make([][]*Cell, g.rows)
+
+	for r := 0; r < g.rows; r++ {
+		g.cells[r] = make([]*Cell, g.columns)
+
+		for c := 0; c < g.columns; c++ {
+			g.cells[r][c] = NewCell(r, c)
+		}
+	}
+}
+
+func (g *Grid) configureCells() {
+	for r := 0; r < g.rows; r++ {
+		for c := 0; c < g.columns; c++ {
+			cell, err := g.Cell(r, c)
+			if err != nil {
+				log.Fatalf("failed to initialize grid: %v", err)
+			}
+			// error is ignored, we just set nil if there is no neighbor
+			cell.north, _ = g.Cell(r-1, c)
+			cell.south, _ = g.Cell(r+1, c)
+			cell.west, _ = g.Cell(r, c-1)
+			cell.east, _ = g.Cell(r, c+1)
+		}
+	}
+}
+
+// Cell returns the cell at r,c
+func (g *Grid) Cell(r, c int) (*Cell, error) {
+	if r < 0 || r >= g.rows || c < 0 || c >= g.columns {
+		return nil, fmt.Errorf("(%v, %v) is outside the grid", r, c)
+	}
+	return g.cells[r][c], nil
+}
+
+// RandomCell returns a random cell
+func (g *Grid) RandomCell() *Cell {
+	return g.cells[random(0, g.rows)][random(0, g.columns)]
+}
+
+// Size returns the number of cells in the grid
+func (g *Grid) Size() int {
+	return g.rows * g.columns
+}
+
+// Rows returns a list of rows (essentially the grid
+func (g *Grid) Rows() [][]*Cell {
+	return g.cells
+}
+
+// Cells returns a list of cells in the grid
+// TODO(dan): Make this into a generator?
+func (g *Grid) Cells() []*Cell {
+	cells := []*Cell{}
+	for r := 0; r < g.rows; r++ {
+		for c := 0; c < g.columns; c++ {
+			cells = append(cells, g.cells[r][c])
+		}
+	}
+	return cells
+}
+
+// Cell defines a single cell in the grid
+type Cell struct {
+	row, column int
+	// keep track of neighborgs
+	north, south, east, west *Cell
+	// keeps track of which cells this cell has a connection (no wall) to
+	links map[*Cell]bool
+}
+
+// NewCell initializes a new cell
+func NewCell(r, c int) *Cell {
+	return &Cell{
+		row:    r,
+		column: c,
+		links:  make(map[*Cell]bool),
+	}
+}
+
+func (c *Cell) String() string {
+	return fmt.Sprintf("(%v, %v)", c.row, c.column)
+}
+
+func (c *Cell) linkOneWay(cell *Cell) {
+	c.links[cell] = true
+}
+
+func (c *Cell) unLinkOneWay(cell *Cell) {
+	delete(c.links, cell)
+}
+
+// Link links a cell to its neighbor (adds passage)
+func (c *Cell) Link(cell *Cell) {
+	c.linkOneWay(cell)
+	cell.linkOneWay(c)
+}
+
+// UnLink unlinks a cell from its neighbor (removes passage)
+func (c *Cell) UnLink(cell *Cell) {
+	c.unLinkOneWay(cell)
+	cell.unLinkOneWay(c)
+}
+
+// Links returns a list of all cells linked to this one
+func (c *Cell) Links() []*Cell {
+	keys := make([]*Cell, len(c.links))
+	i := 0
+	for k := range c.links {
+		keys[i] = k
+		i++
+	}
+	return keys
+}
+
+// Linked returns true if the two cells are linked (joined by a passage)
+func (c *Cell) Linked(cell *Cell) bool {
+	_, linked := c.links[cell] // linked if in the map
+	return linked
+}
+
+// Neighbors returns a list of all cells that are neighbors (weather connected by passage or not)
+func (c *Cell) Neighbors() []*Cell {
+	n := []*Cell{}
+	for _, cell := range []*Cell{c.north, c.south, c.east, c.west} {
+		if cell != nil {
+			n = append(n, c.north)
+		}
+	}
+	return n
+}
