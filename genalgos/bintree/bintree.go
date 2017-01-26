6b070bb3bb09908e572cb604a394484c8655f89b
Dan Tulovsky <dtulovsky@dtulovsky-macpro2.roam.corp.google.com>
2017-01-26 10:58:01 -0500

    more files

---
+++ genalgos/bintree/bintree.go
@@ -0,0 +1,30 @@
+package bintree
+
+import (
+	"mazes/grid"
+	"mazes/utils"
+)
+
+// Apply applies the binary tree algorithm to the grid.
+func Apply(g *grid.Grid) *grid.Grid {
+
+	for _, cell := range g.Cells() {
+		neighbors := []*grid.Cell{}
+		if cell.North != nil {
+			neighbors = append(neighbors, cell.North)
+		}
+		if cell.East != nil {
+			neighbors = append(neighbors, cell.East)
+		}
+
+		if len(neighbors) == 0 {
+			continue
+		}
+		index := utils.Random(0, len(neighbors))
+		neighbor := neighbors[index]
+		if neighbor != nil {
+			cell.Link(neighbor)
+		}
+	}
+	return g
+}
--- generate/genmaze.go
+++ generate/genmaze.go
@@ -11,6 +11,5 @@ func main() {
 	g := grid.NewGrid(10, 10)
 	g = bintree.Apply(g)
 	fmt.Printf("%v\n", g)
-	// something
 
 }
