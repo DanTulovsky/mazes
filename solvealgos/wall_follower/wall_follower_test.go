7db8204ee97ab392b84617f4a853a94712668d33
Daniel Tulovsky <dant@imacz.lan>
2017-02-10 19:29:53 -0500

    fix tests

--- solvealgos/wall_follower/wall_follower_test.go
+++ solvealgos/wall_follower/wall_follower_test.go
@@ -68,7 +68,7 @@ func TestSolveAldousBroder(t *testing.T) {
 
 func TestSolveRecursiveBacktracker(t *testing.T) {
 	for _, tt := range applytests {
-		g, err := maze.NewGrid(tt.config)
+		m, err := maze.NewGrid(tt.config)
 		gen, solv := &recursive_backtracker.RecursiveBacktracker{}, &WallFollower{}
 
 		if err != nil {
@@ -81,37 +81,37 @@ func TestSolveRecursiveBacktracker(t *testing.T) {
 
 		// orphan cells
 		for _, l := range tt.orphanCells {
-			cell, err := g.Cell(l.X, l.Y)
+			cell, err := m.Cell(l.X, l.Y)
 			if err != nil {
 				t.Fatalf(err.Error())
 			}
 			cell.Orphan()
 		}
 
-		if g, err = gen.Apply(g, 0); err != nil {
+		if m, err = gen.Apply(m, 0); err != nil {
 			t.Errorf("apply failed: %v", err)
 		}
 
-		if err := gen.CheckGrid(g); err != nil {
-			fmt.Printf("%v\n", g)
+		if err := gen.CheckGrid(m); err != nil {
+			fmt.Printf("%v\n", m)
 			t.Fatalf("grid is not valid: %v", err)
 		}
 
-		g.ResetVisited()
-		fromCell := g.RandomCell()
-		toCell := g.RandomCell()
-		if g, err = solv.Solve(g, fromCell, toCell, 0); err != nil {
-			log.Printf("\n%v\n", g)
+		m.ResetVisited()
+		fromCell := m.RandomCell()
+		toCell := m.RandomCell()
+		if m, err = solv.Solve(m, fromCell, toCell, 0); err != nil {
+			log.Printf("\n%v\n", m)
 			t.Fatalf("failed to solve: %v", err)
 		}
 
-		for _, o := range g.OrphanCells() {
+		for o := range m.OrphanCells() {
 			// make sure orphan cells are not in the solution
-			if maze.CellInCellList(o, g.SolvePath.ListCells()) {
-				t.Errorf("orpha cell %v is in solvePath [%v]", o, g.SolvePath)
+			if maze.CellInCellList(o, m.SolvePath.ListCells()) {
+				t.Errorf("orpha cell %v is in solvePath [%v]", o, m.SolvePath)
 			}
-			if maze.CellInCellList(o, g.TravelPath.ListCells()) {
-				t.Errorf("orpha cell %v is in travelPath [%v]", o, g.TravelPath)
+			if maze.CellInCellList(o, m.TravelPath.ListCells()) {
+				t.Errorf("orpha cell %v is in travelPath [%v]", o, m.TravelPath)
 			}
 		}
 	}
